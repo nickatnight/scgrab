eaba675e24e7c5cc650bb5c92aad9af65bb96f47
nickatnight <kay.muneee@gmail.com>
2016-04-04 20:11:26 -0700

    added update id3 tags for downloaded track

--- scgrab.py
+++ scgrab.py
@@ -5,15 +5,29 @@ import mechanize
 import pbar
 import sys
 from colorme import ColorMe
+from mutagen import File
 from track import TrackInfo
 
 
 class SoundCloudDL(object):
     """
-    Convert and download SoundCloud links to Mp3's right on your dekstop.
+    Convert and download SoundCloud links to Mp3's right to your dekstop.
 
     Attributes:
         song_link: link extension that is input from the user at command line
+        br: browser
+        sc_site: soundcloud url string
+        url: track url
+        tags: TrackInfo object
+        cookiejar: Auotmatic cookie handling
+
+        ****Browser Settings****
+        set_handle_equiv: Dont handle HTTP-EQUIV Headers
+        set_handle_redirect:
+        set_handle_referer: See original page request
+        set_handle_robots: Ignore robots.txt
+        set_handle_refresh: Allow 1 refresh redirect
+        addheaders: Prepend necessary hearders for page req
     """
     def __init__(self, song_link):
         self.song_link = song_link
@@ -21,21 +35,15 @@ class SoundCloudDL(object):
         self.br = mechanize.Browser()
         self.sc_site = 'https://soundcloud.com/'
         self.url = self.sc_site + song_link
-        self.json_response = {}
+        self.tags = None # TrackInfo object
         self.cookiejar = cookielib.LWPCookieJar()
         self.br.set_cookiejar(self.cookiejar)
 
-        # Don't handle HTTP-EQUIV headers
         self.br.set_handle_equiv(True)
-
         self.br.set_handle_redirect(True)
-        # See original page request
         self.br.set_handle_referer(True)
-        # Ignore robots.txt...
         self.br.set_handle_robots(False)
-        # Allow 1 refresh redirect
         self.br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
-        # Headers for browser
         self.br.addheaders = [ ( 'User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1' ) ]
 
 
@@ -81,13 +89,9 @@ class SoundCloudDL(object):
 
     def fetch_data(self):
         """
-        Generate data from the SoundCloud api response. Must have teh client ID
+        Generate data from the SoundCloud api response. Must have the client ID
         in order to authorize the request.
         """
-
-        # Need to fix this
-        global track_id
-        tags = None
 
         # This response will generate JSON data that includes the all track
         # information.
@@ -101,11 +105,9 @@ class SoundCloudDL(object):
         j = json.loads(resp.read())
 
         # Parse the data to retrive only what we need
-        tags = self.parse_data(j)
+        self.tags = self.parse_data(j)
 
-        #self.id3_tag(tags)
-
-        stream_api = 'http://api.soundcloud.com/tracks/' + tags._id + \
+        stream_api = 'http://api.soundcloud.com/tracks/' + self.tags._id + \
             '/stream?client_id=' + keys.ID
 
         self.page_load(stream_api)
@@ -138,8 +140,20 @@ class SoundCloudDL(object):
 
         ColorMe.color_text('\nDownload complete.', 'ok')
 
-    def id3_tag(self):
-        pass
+    def update_id3_tags(self):
+        """
+        Update ID3 tags of downloaded song
+        """
+
+        # Must init ID3 tags for appropriate header settings
+        audio = File("test.mp3", easy=True)
+        audio.add_tags()
+
+        # Save new tags
+        audio['title'] = self.tags._title
+        audio['artist'] = self.tags._artist
+        audio['album'] = self.tags._album
+        audio.save('test.mp3')
 
     def run(self):
         """
@@ -152,6 +166,8 @@ class SoundCloudDL(object):
         self.fetch_data()
 
         self.download()
+
+        self.update_id3_tags()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
